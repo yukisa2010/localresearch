1d0587d18df67eeac03adb3df6fcc62f5d587def
yukisa2010 <43121712+yukisa2010@users.noreply.github.com>
2019-07-21 10:09:47 +0900

    memo.js amendment

--- src/Memo.js
+++ src/Memo.js
@@ -2,9 +2,19 @@ import React from 'react'
 
 
 const Memo = () => (
-  <div>
-    <textarea>めも</textarea>
-    <button>登録</button>
+  <div className='memo-wrapper'>
+    <div className='memo'>
+      <p>追加記入欄</p>
+      <label>担当者</label><br/>
+      <select>
+        <option selected>名前を選択してください</option>
+        <option>社員001</option>
+        <option>ゲスト001</option>
+      </select><br/>
+      <label>エリア<br/><input type='text' id='area' /></label><br/>
+      <label>コメント<br/><input type='text' id='comment' /></label><br/>
+      <button>登録</button>
+    </div>
   </div>
 )
 
--- src/Thanks.js
+++ src/Thanks.js
@@ -6,7 +6,7 @@ const Thanks = () => (
     <p>Thanks!</p>
     <p>アンケートに回答頂きありがとうございました。</p>
     <Link to='/memo' style={{ textDecoration: 'none' }}><button id='addbtn'>追加入力</button></Link><br/>
-    <Link to='/'  style={{ textDecoration: 'none' }}><button id='backbtn'>戻る</button></Link><br/>
+    <Link to='/' style={{ textDecoration: 'none' }}><button id='backbtn'>戻る</button></Link><br/>
   </div>
 )
 
