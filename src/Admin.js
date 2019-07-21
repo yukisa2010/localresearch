0d96c856886898250ad958ff90c683e33fb1ed28
yukisa2010 <43121712+yukisa2010@users.noreply.github.com>
2019-07-21 18:15:31 +0900

    created JSON API

--- src/Admin.js
+++ src/Admin.js
@@ -2,21 +2,25 @@ import React from 'react'
 import firebase from './firebase'
 
 const dbRef = firebase.database().ref();
-dbRef.on('value', snapshot => {
-  const admin = document.getElementById('admin')
 
-  if (snapshot.val() !== null){
-    admin.innerText = JSON.stringify(snapshot.val())
-    return
-
-  } else {
-    return
+dbRef.on(
+  'value', 
+  snapshot => {
+  //firebaseから取得した値をJSONにする
+  const obj = JSON.stringify(snapshot.val());
+  //JSONを取得できるようにBLOBに加工する
+  const jsonfile = new Blob([JSON.stringify(obj, null, '  ')], {type: 'application/json'});
+  //JSONをダウンロードできるURLを生成
+  const url = URL.createObjectURL(jsonfile)
+  //URLをaタグへと紐づける
+  document.getElementById('download').href = url
   }
-});
-
+  )
 
 const Admin = () => (
-  <div id='admin'></div>
+  <div>
+    <a id='download'>Download JSON</a>
+  </div>
 )
 
 
