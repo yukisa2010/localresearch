3db2759f90f44d75eb25d3abfc37bd2a6a4f807c
yukisa2010 <43121712+yukisa2010@users.noreply.github.com>
2019-07-28 16:15:43 +0900

    aboslute position of giftItems has changed

--- src/TableComponent.js
+++ src/TableComponent.js
@@ -28,11 +28,11 @@ const TableComponent = () => {
   return (
   <div className='giftItems'>
     <GiftItem flag={itemName[0]} url={imgUrl[0]} labelcolor='black' />
-    <GiftItem flag={itemName[1]} url={imgUrl[1]} labelcolor='black'/>
-    <GiftItem flag={itemName[2]} url={imgUrl[2]} labelcolor='black'/>
-    <GiftItem flag={itemName[3]} url={imgUrl[3]} labelcolor='white'/>
-    <GiftItem flag={itemName[4]} url={imgUrl[4]} labelcolor='black'/>
-    <GiftItem flag={itemName[5]} url={imgUrl[5]} labelcolor='black'/>
+    <GiftItem flag={itemName[1]} url={imgUrl[1]} labelcolor='black' />
+    <GiftItem flag={itemName[2]} url={imgUrl[2]} labelcolor='black' />
+    <GiftItem flag={itemName[3]} url={imgUrl[3]} labelcolor='white' />
+    <GiftItem flag={itemName[4]} url={imgUrl[4]} labelcolor='black' />
+    <GiftItem flag={itemName[5]} url={imgUrl[5]} labelcolor='black' />
   </div>
   )
 };
