e9a42041986d140d58f5a109bf3d8c32eb1046bc
yukisa2010 <43121712+yukisa2010@users.noreply.github.com>
2019-08-01 06:37:24 +0900

    table added

--- src/View.js
+++ src/View.js
@@ -1,23 +1,63 @@
 import React from 'react'
+import ReactDOM from 'react-dom'
 import firebase from './firebase';
+
 
 
 
 const View = () => {
   const dbRef = firebase.database().ref();
+  const headerArr = ["id", "名前", "月", "年", "日", "郵便番号", "住所1", "住所2", "備考",   "担当者", "景品", "最終更新日時",  "電話番号", "会社","エリア"]
+  const header = <tr>{headerArr.map(head => <th>{head}</th>)}</tr>
+  let recordItems = []
+  let entireTable  
+
+
+
 
   dbRef.on(
     'value', 
-    snapshot => (
-      document.getElementById('data').textContent = JSON.stringify(snapshot.val())
-    )
-  );
+    snapshot => {
+      var obj = snapshot.val();
+      var objKeys = Object.keys(obj);
 
+      //レコード単位
+      for(var i=0;i<objKeys.length;i++){
+        var id = objKeys[i]
+        var record = obj[id];
+        var tdItems = [];
+
+        //フィールド単位
+        for(var j=0;j<headerArr.length;j++){  
+          tdItems.push(record[headerArr[j]])
+        }
+        recordItems.push(<tr key={i}>{tdItems.map(tdItem => (<td>{tdItem}</td>))}</tr>)        
+      }
+      entireTable = (
+        <table>
+          <thead>
+            {header}
+          </thead>
+          <tbody>
+            {recordItems}
+          </tbody>
+        </table>
+      )
+
+
+      ReactDOM.render(entireTable, document.getElementById('data'))
+
+      // setData(recordItem)
+
+    }
+
+  )
 
   return (
     <div>
       <p>View</p>
-      <div id='data'></div>
+      <div id='data'>
+      </div>
     </div>
   )
 }
