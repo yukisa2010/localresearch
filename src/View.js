ace5b811bbed00bca3905070b6677ef390c3934a
yukisa2010 <43121712+yukisa2010@users.noreply.github.com>
2019-08-01 07:04:05 +0900

    table id numbered

--- src/View.js
+++ src/View.js
@@ -3,11 +3,9 @@ import ReactDOM from 'react-dom'
 import firebase from './firebase';
 
 
-
-
 const View = () => {
   const dbRef = firebase.database().ref();
-  const headerArr = ["id", "名前", "月", "年", "日", "郵便番号", "住所1", "住所2", "備考",   "担当者", "景品", "最終更新日時",  "電話番号", "会社","エリア"]
+  const headerArr = ["id", "名前", "年","月", "日", "郵便番号", "住所1", "住所2", "備考",   "担当者", "景品", "最終更新日時","電話",  "電話番号", "会社","エリア"]
   const header = <tr>{headerArr.map(head => <th>{head}</th>)}</tr>
   let recordItems = []
   let entireTable  
@@ -29,7 +27,11 @@ const View = () => {
 
         //フィールド単位
         for(var j=0;j<headerArr.length;j++){  
-          tdItems.push(record[headerArr[j]])
+          if(headerArr[j] === 'id'){
+            tdItems.push((i+1).toString())
+          }else{
+            tdItems.push(record[headerArr[j]])
+          }
         }
         recordItems.push(<tr key={i}>{tdItems.map(tdItem => (<td>{tdItem}</td>))}</tr>)        
       }
